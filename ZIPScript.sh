a7cc497dfe9377373dc2f5501cd1a6b317e1eb4f
Michael Esparza <michael.l.esparza@gmail.com>
2015-02-08 21:16:48 -0600

    Fixed tempfolder creation
    Was creating folders in wrong place and was nesting them until completion.
    
    rm -rf is there to cleanup anything that may have been there before. 
    
    Use with caution.

--- ZIPScript.sh
+++ ZIPScript.sh
@@ -4,7 +4,7 @@ pwd=$(pwd)
 #echo $pwd
 for dir in "$pwd"/*;
 do
-for subdir in "$dir"/*;
+    for subdir in "$dir"/*;
     do
         #echo ${subdir}
         for file in "$subdir"/*;
@@ -14,8 +14,9 @@ for subdir in "$dir"/*;
                 # now for each of these index.htmls I want to go into it and download the zip
                 zip=$(grep ".zip" "$file" | grep -o '<a .*href=.*>' | sed 's/[^"]*"\([^"]*\).*/\1/')
                 #zip=$(grep ".zip" "$file" | grep -Po '(?<=a href=).+' "$file")
-                mkdir tempfolder
-                cd tempfolder
+                rm -rf "$subdir"/tempfolder
+                mkdir "$subdir"/tempfolder
+                cd "$subdir"/tempfolder
                 curl -sS "$zip" > temp.zip
                 unzip temp.zip
                 rm temp.zip
